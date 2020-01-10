e447da4577440b45cd92f654e162b0b6fc95a5ff
Calyx <calyx@pathplanner.app>
2020-01-10 09:58:50 +0000

    TS definitions

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,4 @@
-export interface KeyInfo {
-  Modifiers: ModifierInfo;
-  Key: string;
-}
-export interface ModifierInfo {
-  Control: boolean;
-  Shift: boolean;
-  Alt: boolean;
-}
-export function addHook(callback: (keyInfo: KeyInfo) => void) : void;
-export function clearHook() : void;
+export function beginListener(): void;
+export function removeListener(): void;
+
+export function register(shortcut, callback): void;
